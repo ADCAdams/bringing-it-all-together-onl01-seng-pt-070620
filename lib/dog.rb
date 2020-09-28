827b67dbce4a880f00292bd5a5f570a7380b7dbc
Andy Adams <andrewdcadams@gmail.com>
2020-09-28 22:45:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,9 +32,9 @@ class Dog
   
   
   def self.new_from_db(array)
-    id = array[0]
-    name = array[1]
-    breed = array[2]
+    id = array[0][1]
+    name = array[0][1]
+    breed = array[0][2]
     
     self.new(id: id, name: name, breed: breed)
   end
